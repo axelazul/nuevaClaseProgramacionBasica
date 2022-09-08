945b2fd117fb43b6986577dccfba0d8ca3906159
axelazul <axel.palacios@hotmail.com>
2022-09-08 15:18:19 -0300

    Pequeños Fix

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
 function  aleatorio (min, max){
     return Math.floor(Math.random()* (max-min +1) + min)
 }
+
+function eleccion (jugada){
+    let resultado = "";
+    if (jugada == 1){
+        resultado = "Piedra ";
+    } else if ( jugada == 2) {
+        resultado = "Papel ";
+    } else if (jugada == 3){
+        resultado = "Tijera ";
+    } else {
+        resultado = "MAL ELEGIDO!!";
+    }
+    return resultado;
+}
+
 // 1 es piedra, 2 es papel, 3 es tijera 
 let jugador = 0;
 let pc = aleatorio(1,3);
 jugador = prompt("Elige 1 para piedra, 2 para papel y 3 para tijera :");
 //alert("Eegiste: " + jugador);
-if (jugador == 1){
-    alert("Elegiste Rock");
-} else if ( jugador == 2) {
-    alert("Elegiste Paper");
-} else if (jugador == 3){
-    alert("Elegiste Sissors");
-} else {
-    alert ("Elegiste mal");
-}
-if (pc == 1){
-    alert("PC elige Rock");
-} else if ( pc == 2) {
-    alert("PC elige Paper");
-} else if (pc == 3){
-    alert("PC elige Sissors");
-} else {
-    alert ("PC eligió mal");
-}
+
+alert("PC Elige :" + eleccion(pc));
+alert("Tu Eliges :" + eleccion(jugador));
+
 // COMBATE
 if ( pc == jugador){
     alert("Empate");
