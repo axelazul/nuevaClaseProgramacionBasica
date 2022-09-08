eed969ac6534bb71b302ff5091e5f56446f66a3f
axelazul <axel.palacios@hotmail.com>
2022-09-08 15:42:29 -0300

    Agregado Ciclo While a Piedra Papel Tijera

--- main.js
+++ main.js
@@ -5,35 +5,47 @@ function  aleatorio (min, max){
 function eleccion (jugada){
     let resultado = "";
     if (jugada == 1){
-        resultado = "Piedra ";
+        resultado = "Piedra 🥌 ";
     } else if ( jugada == 2) {
-        resultado = "Papel ";
+        resultado = "Papel 📋";
     } else if (jugada == 3){
-        resultado = "Tijera ";
+        resultado = "Tijera ✂";
     } else {
-        resultado = "MAL ELEGIDO!!";
+        resultado = "MAL ELEGIDO!! ☠⚰";
     }
     return resultado;
 }
 
 // 1 es piedra, 2 es papel, 3 es tijera 
 let jugador = 0;
-let pc = aleatorio(1,3);
-jugador = prompt("Elige 1 para piedra, 2 para papel y 3 para tijera :");
-//alert("Eegiste: " + jugador);
+let pc = 0;
+let triunfos = 0;
+let perdidas = 0;
 
-alert("PC Elige :" + eleccion(pc));
-alert("Tu Eliges :" + eleccion(jugador));
+while (triunfos < 3 && perdidas < 3) {
+    pc = aleatorio(1,3);
+    jugador = prompt("Elige 1 para piedra 🥌, 2 para papel  📋y 3 para tijera ✂:");
+    //alert("Eegiste: " + jugador);
 
-// COMBATE
-if ( pc == jugador){
-    alert("Empate");
-} else if ( jugador == 1 && pc == 3){
-    alert("Ganaste!!");
-} else if ( jugador == 2 && pc == 1){
-    alert("Ganaste!!");
-} else if ( jugador == 3 && pc == 2 ){
-    alert("Ganaste!!");
-} else {
-    alert("PERDISTE!!");
+    alert("PC Elige :" + eleccion(pc));
+    alert("Tu Eliges :" + eleccion(jugador));
+
+    // COMBATE
+    if ( pc == jugador){
+        alert("EMPATE 🥱");
+    } else if ( jugador == 1 && pc == 3){
+        alert("GANASTE!! 😎");
+        triunfos = triunfos + 1;
+    } else if ( jugador == 2 && pc == 1){
+        alert("GANASTE!! 😎");
+        triunfo = triunfos + 1;
+    } else if ( jugador == 3 && pc == 2 ){
+        alert("GANASTE!! 😎");
+        triunfos = triunfos + 1;
+    } else {
+        alert("PERDISTE!! 😰");
+        perdidas = perdidas + 1;
+    }
 }
+
+alert ("Ganaste " + triunfos+ " veces. Perdiste " + perdidas + " veces");
