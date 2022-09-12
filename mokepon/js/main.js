e74e4ebad950517fe6e2c0cf7ca48323a0bedf67
axelazul <axel.palacios@hotmail.com>
2022-09-12 17:32:48 -0300

    RECORDAR ARREGLAR CODIGO git add .

--- mokepon/js/main.js
+++ mokepon/js/main.js
@@ -25,5 +25,30 @@ function seleccionarMascotaJugador() {
         alert("ELIGE TU MASCOTA!!");
         
     }
+
+    seleccionarMascotaEnemigo()
 }
+
+
+    function seleccionarMascotaEnemigo (){
+        let ataqueAleatorio = aleatorio(1,5);
+        let spanMascotaEnemigo = document.getElementById("mascota-enemigo");
+
+        if (ataqueAleatorio == 1){
+            spanMascotaEnemigo.innerHTML == "Hipodoge";
+        } else if (ataqueAleatorio == 2){
+            spanMascotaEnemigo.innerHTML == "Capipepo";
+        } else if ( ataqueAleatorio == 3){
+            spanMascotaEnemigo == "Ratigueya";
+        } else if ( ataqueAleatorio == 4){
+            spanMascotaEnemigo.innerHTML == "Tucapalma";
+        } else {
+            spanMascotaEnemigo.innerHTML == "Pydos";
+        }
+    }
+
+    function  aleatorio (min, max){
+        return Math.floor(Math.random()* (max-min +1) + min)
+    }
+
 window.addEventListener("load", iniciarJuego); //ESTA LINEA AYUDA A QUE AUNQUE EL LLAMADO SCRIPT DE HTML ESTE AL PRINCIPIO, SE PUEDA CARGAR TODA DE LA PAGINA Y SE EJECUTE DE  BUENA MANERA EL SCRIPT SIN LA NECESIDAD DE COLOCARLO ABAJO EN EL HTML//
